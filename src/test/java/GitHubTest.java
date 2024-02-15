bd8cfaa0bdc7d062703e6c96c3ccdf8ec4759aba
Ekaterina Kladkova <e.kladkova@qiwi.com>
2024-02-15 20:39:14 +0300

    enterpriseCheck new autotest

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -3,8 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTest {
@@ -13,16 +12,22 @@ public class GitHubTest {
     static void beforeAll() {
         Configuration.browser = "firefox";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
     }
 
+    // HW4
     @Test
     void softAssertionsJUnit5CodeCheck() {
+        // Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
+        // Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $(".wiki-rightbar").$(withText("more pages")).click();
         $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        // Откройте страницу SoftAssertions
         $$(".js-wiki-sidebar-toggle-display li").findBy(text("SoftAssertions")).$("a").click();
+        // Проверьте что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
         $(".markdown-body").shouldHave(text(""" 
                                 @ExtendWith({SoftAssertsExtension.class})
@@ -37,6 +42,17 @@ public class GitHubTest {
                                   }
                                 }
                                 """)); // проверка примера кода многострочным литералом
+    }
+
+    // HW5
+    @Test
+    void enterpriseCheck() {
+        open("https://github.com");
+        // На главной странице GitHub выберите меню Solutions -> Enterprize с помощью команды hover для Solutions
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(byText("Enterprise")).click(); // setTimeout(function () {debugger}, 4000)
+        // Убедитесь что загрузилась нужная страница
+        $("#hero-section-brand-heading").shouldHave(text("developer platform"));
     }
 
 }
