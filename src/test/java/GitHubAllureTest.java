e197262a68c463229724936358f4822b6d8f6148
Ekaterina Kladkova <e.kladkova@qiwi.com>
2024-03-11 21:32:30 +0300

    deleted unused imports

--- src/test/java/GitHubAllureTest.java
+++ src/test/java/GitHubAllureTest.java
@@ -1,12 +1,9 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
